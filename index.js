afb17a840626d4ffbde0ec5029a59c11ba8d9792
sylkm <47570912+sylkm@users.noreply.github.com>
2021-11-07 01:18:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const url = 'https://json-server--3000.local.webcontainer.io/comments';
+var port = Process.env.PORT || 3000;
+const url = 'https://json-server' + port + '+--.local.webcontainer.io/comments';
 
 fetch(url)
-  .then(r => r.json())
-  .then(json => (document.getElementById('output').innerHTML = JSON.stringify(json, null, 2)));
+  .then((r) => r.json())
+  .then(
+    (json) =>
+      (document.getElementById('output').innerHTML = JSON.stringify(
+        json,
+        null,
+        2
+      ))
+  );
